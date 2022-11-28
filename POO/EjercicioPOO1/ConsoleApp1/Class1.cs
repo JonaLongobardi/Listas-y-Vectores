7a402160859225f027e81f84e98f610b047df748
JonaLongobardi <118371962+JonaLongobardi@users.noreply.github.com>
2022-11-28 13:43:32 -0300

    Add files via upload

---
+++ POO/EjercicioPOO1/ConsoleApp1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPoo
+{
+    class Auto
+    {
+        private string Color;
+        private string Marca;
+        private string Modelo;
+        private string Patente;
+        private int CantPuertas;
+
+         
+    }
+}
